16b2397b00776d0569e527a3e44e777e7eea459f
Aditya Sharma <165514237+codeAditer@users.noreply.github.com>
2026-07-31 16:04:48 +0530

    Time: 1 ms (99.97%), Space: 42.7 MB (26.86%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -2,15 +2,18 @@ class Solution {
     public int reverse(int x) {
         int revNum = 0;
         while(x!=0){
+            //modular give the lastd digit of x
             int lastD = x%10;
+            //check edge case for not limit exceed
             if(revNum>Integer.MAX_VALUE/10){
                 return 0;
             }
             if(revNum<Integer.MIN_VALUE/10){
                 return 0;
             }
-            
+            //multiply with 10 and add then last digit 
             revNum = (revNum*10)+lastD;
+            
             x = x/10;
         }
        return revNum; 
